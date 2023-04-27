6d372eb37e42284df991c2a621e7627d5dcbe7a6
William Veal Phan <william.veal-phan@ledger.fr>
2023-04-27 16:00:36 +0200

    feat: add create boss for admin only

--- src/Game.sol
+++ src/Game.sol
@@ -1,18 +1,64 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.13;
 
+/// @notice error thrown when a function is called by without the appropriate rights
+error Unauthorized();
+
+/// @title Admin rights
+/// @author William Veal Phan
+/// @notice Define modifiers and functions to manage admin rights
+/// @dev Inhirit from this contract and set admin address
+contract AdminRights {
+    /// @notice address with admin permissions
+    address public admin;
+
+    modifier onlyAdmin() {
+        if (msg.sender != admin) revert Unauthorized();
+        _;
+    }
+}
+
+struct Boss {
+    uint256 hp;
+    uint256 damage;
+    uint256 reward;
+}
+
+struct Character {
+    uint256 hp;
+    uint256 damage;
+    uint256 reward;
+}
+
 /// @title World of Ledger
 /// @author William Veal Phan
-contract Game {
+contract Game is AdminRights {
     /// @notice owner of the contract
     /// @dev owner never changes
     address public immutable owner;
 
-    /// @notice address with admin permissions
-    address public admin;
+    /// @dev id of the last boss created; if eq 0, no boss exists
+    uint256 bossId = 0;
+
+    /// @notice bosses currently in the game
+    mapping(uint256 => Boss) bosses;
 
     constructor() {
+        // owner is the deployer of this contract
         owner = msg.sender;
+        // owner inherit admin rights by default
         admin = msg.sender;
+    }
+
+    /// @notice create a custom boss and add it to the game; need admin rights
+    /// @dev increment bossId and add a new boss to the bosses mapping
+    /// @param _hp new boss starting hp
+    /// @param _damage new boss damage
+    /// @param _reward new boss reward
+    /// @return bossId ID of the newly created boss
+    function createBoss(uint256 _hp, uint256 _damage, uint256 _reward) external onlyAdmin returns (uint256) {
+        bossId++;
+        bosses[bossId] = Boss(_hp, _damage, _reward);
+        return bossId;
     }
 }
--- test/Game.t.sol
+++ test/Game.t.sol
@@ -11,11 +11,28 @@ contract GameTest is Test {
         game = new Game();
     }
 
-    function testOwner() public {
+    function test_OwnerIsDeployer() public {
         assertEq(address(game.owner()), address(this));
     }
 
-    function testAdmin() public {
+    function test_OwnerIsAdmin() public {
         assertEq(address(game.admin()), game.owner());
+    }
+
+    function test_CreateBoss(uint256 _hp, uint256 _damage, uint256 _reward) public {
+        uint256 bossId = game.createBoss(_hp, _damage, _reward);
+        assertEq(bossId, 1);
+    }
+
+    function test_RevertWhen_CallAddBossWithoutAdminRights(
+        address _notOwner,
+        uint256 _hp,
+        uint256 _damage,
+        uint256 _reward
+    ) public {
+        vm.assume(address(game.admin()) != address(_notOwner));
+        vm.expectRevert(Unauthorized.selector);
+        vm.prank(address(_notOwner));
+        game.createBoss(_hp, _damage, _reward);
     }
 }
